c134b78a8206ed8527995a764142c7f65db8ee8f
Bodenner <204929239+tbodenner@users.noreply.github.com>
2025-08-01 14:13:07 -0700

    Added csharp .net solution folder.

---
+++ PrescottOITShipping/Model/ShippingAddress.cs
@@ -0,0 +1,18 @@
+﻿namespace PrescottOITShipping.Model
+{
+    class ShippingAddress(string name, string address, string state, string city, string zip)
+    {
+        // properties
+        private readonly string _name = name;
+        private readonly string _address = address;
+        private readonly string _city = city;
+        private readonly string _state = state;
+        private readonly string _zipcode = zip;
+
+        public string FullAddress()
+        {
+            string nl = System.Environment.NewLine;
+            return $"{_name}{nl}{_address}{nl}{_city}, {_state} {_zipcode}";
+        }
+    }
+}
---
+++ PrescottOITShipping/Model/SqliteReader.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.Data.Sqlite;
+using PrescottOITShipping.Model;
+using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace PrescottOITShipping.Controller
+{
+    class SqliteReader
+    {
+        private readonly SqliteConnection _connection;
+        private static readonly string _sqliteFilename = "Addresses.db";
+        private readonly List<ShippingAddress> _shippingAddresses;
+        public SqliteReader()
+        {
+            // create our connection string
+            string connectionString = $"Data Source={_sqliteFilename}";
+            // create the connection with our connection string
+            _connection = new SqliteConnection(connectionString);
+            // get our addresses
+            _shippingAddresses = getRowsFromDatabase();
+        }
+
+        private List<ShippingAddress> getRowsFromDatabase()
+        {
+            // the command to read all our rows
+            string sqlCommand = "SELECT * FROM Addresses;";
+
+            // try to read from the database
+            try
+            {
+                // open the database
+                _connection.Open();
+                // create our command
+                using (SqliteCommand command = new(sqlCommand))
+                {
+                    // execute our command
+                    using (SqliteDataReader reader = command.ExecuteReader())
+                    {
+                        // read our data
+                        if (reader != null && reader.HasRows)
+                        {
+                            while (reader.Read())
+                            {
+                                string name;
+                                string address;
+                                string city;
+                                string state;
+                                string zip;
+                                // get the entire row
+                                object[] row = new object[reader.FieldCount];
+                                reader.GetValues(row);
+
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+
+            // return an empty list of the return type
+            return [];
+        }
+        // connect to db
+        // read db
+        // create objects
+    }
+}
